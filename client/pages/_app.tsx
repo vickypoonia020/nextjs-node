a29bb14daf95a2cb6d0302d8ce49c38bdda491b9
Vicky Poonia <114798756+vicky-poonia@users.noreply.github.com>
2023-12-09 22:54:02 +0530

    layout addition

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,16 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Header from "@/components/shared/Header";
-import Footer from "@/components/shared/Footer";
+import Layout from "@/components/shared/Layout";
 import "../styles/css/layout.scss";
 import "../styles/css/homepage.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   );
 }
