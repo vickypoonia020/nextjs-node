cb5eb4d83bafb40942d6b11b579a9f815a1b4992
Anjali Sharma <anjali.sharma09149@gmail.com>
2023-12-09 19:41:33 +0530

    Added home-img

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Header from "@/components/shared/Header";
+import Footer from "@/components/shared/Footer";
+import "../styles/css/layout.scss";
+import "../styles/css/homepage.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
