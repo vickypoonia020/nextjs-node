32a4c7195a4655708c30c83abd1d3cacf5fbad61
Vicky Poonia <114798756+vicky-poonia@users.noreply.github.com>
2023-12-09 23:39:40 +0530

    footer added

--- client/components/shared/Footer.tsx
+++ client/components/shared/Footer.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+import Image from "next/image";
+import styles from "../../styles/css/footer.module.scss";
 
 const Footer = () => {
   return (
     <footer>
-      <div className="footer">Footer</div>
+      <div className={styles.footer}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="/images/BG1.jpeg"
+            alt="BG1"
+            width={400}
+            height={400}
+          />
+          <p>Text below BG1 image</p>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src="/images/BG2.jpeg"
+            alt="BG2"
+            width={400}
+            height={400}
+          />
+          <p>Text below BG2 image</p>
+        </div>
+      </div>
     </footer>
   );
 };
