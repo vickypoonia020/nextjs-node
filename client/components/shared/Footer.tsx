d9a4ccafe6effbc4eb4235adadcc20a5e4e5ff37
Vicky Poonia <114798756+vicky-poonia@users.noreply.github.com>
2023-12-30 23:43:57 +0530

    Vicky (#7)
    * footer
    
    * footer
    
    * menu basics

--- client/components/shared/Footer.tsx
+++ client/components/shared/Footer.tsx
@@ -1,27 +1,31 @@
 import React from "react";
 import Image from "next/image";
 import styles from "../../styles/css/footer.module.scss";
+import Link from "next/link";
+import { useEffect } from "react";
 
 const Footer = () => {
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    const scrollButton = document.getElementById("scrollToTop");
+    if (scrollButton) {
+      scrollButton.addEventListener("click", handleScrollToTop);
+    }
+
+    return () => {
+      if (scrollButton) {
+        scrollButton.removeEventListener("click", handleScrollToTop);
+      }
+    };
+  }, [handleScrollToTop]);
   return (
     <footer>
-      {/* <div className={`${styles.wpb_column} ${styles.col_md_12}`}>
-        <h2
-          className={`${styles.section_title} ${styles.align_center} ${styles.mb_70}`}
-        >
-          FIND US
-        </h2>
-      </div>
-      <div className={styles.footer}>
-        <div className={styles.imageContainer}>
-          <Image src="/images/BG1.jpeg" alt="BG1" width={400} height={400} />
-          <p>Text below BG1 image</p>
-        </div>
-        <div className={styles.imageContainer}>
-          <Image src="/images/BG2.jpeg" alt="BG2" width={400} height={400} />
-          <p>Text below BG2 image</p>
-        </div>
-      </div> */}
       <div className={`${styles.row}`}>
         <div className={`${styles.up_column_}`}>
           <h2 className={`${styles.section_title}`}>FIND US</h2>
@@ -100,23 +104,88 @@ const Footer = () => {
             </div>
           </section>
         </div>
-        <div className={styles.footer}>
+      </div>
+      <div className={`${styles.container}`}>
+        <div data-wow-duration="1.5s">
+          <a
+            href="https://www.giulianorestaurant.co.uk/"
+            title="Giuliano Italian Restaurant | Handforth | Cheshire"
+          >
+            <img
+              src="https://www.giulianorestaurant.co.uk/wp-content/uploads/2017/10/giuliano-footer-logo-1.png"
+              width="156"
+              height="60"
+              alt="Giuliano Italian Restaurant | Handforth | Cheshire"
+            />
+          </a>
+        </div>
+
+        <div className={`${styles.Dish}`}>
+          <div className={`${styles.fbDish}`}>
+            <a
+              href="https://www.facebook.com/GiulianoRestaurant"
+              target="_blank"
+            >
+              <img
+                src="https://www.giulianorestaurant.co.uk/wp-content/uploads/2017/10/facebook.png"
+                alt="Facebook logo"
+                title="Like Giuliano's on Facebook"
+                width="60"
+                height="60"
+              />
+            </a>
+            <a
+              href="https://dishcult.com/restaurant/giulianorestaurant?sortOrder=0&amp;page=1&amp;bookingDate=2023-11-29&amp;covers=2&amp;promotionId=0&amp;bookingTime=19%3A30#reviews"
+              target="_blank"
+            >
+              <img
+                src="https://www.giulianorestaurant.co.uk/wp-content/uploads/2023/12/dish-cult-logo.png"
+                alt="DishCult logo"
+                title="Follow Giuliano's on DishCult"
+                width="60"
+                height="60"
+              />
+            </a>
+            <a
+              href="http://www.tripadvisor.co.uk/Restaurant_Review-g499510-d3958460-Reviews-Giuliano-Handforth_Cheshire_England.html"
+              target="_blank"
+            >
+              <img
+                src="https://www.giulianorestaurant.co.uk/wp-content/uploads/2017/10/trip-advisor-icon.png"
+                title="Read our reviews on Trip Advisor"
+                width="60"
+                height="60"
+                alt="Logo"
+              />
+            </a>
+          </div>
+        </div>
+        <div className={`${styles.footer_made}`}>
+          <a id="scrollToTop">
+            <Image
+              src="/images/sort_up.png"
+              alt="call iconB"
+              width={30}
+              height={30}
+              className={styles.image}
+            />
+          </a>
+        </div>
+      </div>
+      {/* <div className={styles.footer}>
           <div className={styles.imageContainer}>
             <Image src="/images/BG1.jpeg" alt="BG1" width={400} height={400} />
-            {/* <p>Text below BG1 image</p> */}
             <div className={`${styles.ci_text}`} style={{ fontSize: "15px" }}>
               121 Wilmslow Road
             </div>
           </div>
           <div className={styles.imageContainer}>
             <Image src="/images/BG2.jpeg" alt="BG2" width={400} height={400} />
-            {/* <p>Text below BG2 image</p> */}
             <div className={`${styles.ci_text}`} style={{ fontSize: "15px" }}>
               344 lmslow Road
             </div>
           </div>
-        </div>
-      </div>
+        </div> */}
     </footer>
   );
 };
